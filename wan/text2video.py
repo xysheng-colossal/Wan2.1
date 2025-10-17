06ceef1bcf33624773226be9ec4f3ecdded7cd46
zhoufan <zhoufan53@huawei.com>
2025-10-17 07:42:04 +0000

    fix wan2.1 vae parallel size adaptive adjustment error

--- wan/text2video.py
+++ wan/text2video.py
@@ -92,9 +92,13 @@ class WanT2V:
             dtype=self.param_dtype)
         if use_vae_parallel:
             all_pp_group_ranks = []
-            for i in range(0, dist.get_world_size() // 8):
-                all_pp_group_ranks.append(list(range(8 * i, 8 * (i + 1))))
-            set_vae_patch_parallel(self.vae.model, 4, 2, all_pp_group_ranks= all_pp_group_ranks, decoder_decode="decoder.forward")
+            if dist.get_world_size() < 8 :
+                all_pp_group_ranks.append(list(range(0, dist.get_world_size())))
+                set_vae_patch_parallel(self.vae.model, dist.get_world_size(), 1, all_pp_group_ranks= all_pp_group_ranks, decoder_decode="decoder.forward")
+            else:
+                for i in range(0, dist.get_world_size() // 8):
+                    all_pp_group_ranks.append(list(range(8 * i, 8 * (i + 1))))
+                set_vae_patch_parallel(self.vae.model, 4, 2, all_pp_group_ranks= all_pp_group_ranks, decoder_decode="decoder.forward")
 
         logging.info(f"Creating WanModel from {checkpoint_dir}")
         self.model = WanModel.from_pretrained(checkpoint_dir, torch_dtype=self.param_dtype)
